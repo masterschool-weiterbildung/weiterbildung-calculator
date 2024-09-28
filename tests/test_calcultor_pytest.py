cc34dccb41b00cd0d549624d573bd36b97aab986
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-09-28 10:58:44 +0200

    Fixing the flake8 - 11

--- tests/test_calcultor_pytest.py
+++ tests/test_calcultor_pytest.py
@@ -2,47 +2,38 @@ from my_package.calculator import calculate
 
 class TestCalculator:
 
-    def setup_method(self,method):
+    def setup_method(self, method):
         print(f"Setting up {method}")
 
-    def teardown_method(self,method):
+    def teardown_method(self, method):
         print(f"Tearing down {method}")
 
     def test_addition(self):
         assert calculate("2+2") == 4
 
-
     def test_addition1(self):
         assert calculate("4+4") == 8
-
 
     def test_subtraction(self):
         assert calculate("4-1") == 3
 
-
     def test_subtraction1(self):
         assert calculate("5-2") == 3
-
 
     def test_multiplication(self):
         assert calculate("1*1") == 1
 
-
     def test_multiplication1(self):
         assert calculate("3*3") == 9
-
 
     def test_division(self):
         assert calculate("9/3") == 3.0
 
-
     def test_division1(self):
         assert calculate("8/2") == 4.0
 
-
     def test_division_remainder(self):
         assert calculate("6~3") == (2, 0)
-
 
     def test_division_remainder1(self):
         assert calculate("7~3") == (2, 1)
